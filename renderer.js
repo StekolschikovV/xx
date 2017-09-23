550f13134c99423feb87223b1d593e7a0b056628
stekolschikov <stekolschikov@mail.ua>
2017-09-23 17:00:25 +0300

    3

--- renderer.js
+++ renderer.js
@@ -1,3 +1,6 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+// Just place this code at the entry point of your application:
+const updater = require('electron-simple-updater');
+updater.init('https://raw.githubusercontent.com/megahertz/electron-simple-updater/master/example/updates.json');
